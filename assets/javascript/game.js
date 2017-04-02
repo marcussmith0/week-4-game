33aeac7118d8a7f63f7d8cd9be013013b97523de
Marcus Smith <marcussmith0711@gmail.com>
2017-04-02 11:32:31 -0500

    problem in health points

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,13 +10,14 @@
 
 
 
+
 		} ;
 
 		var maul = {
 
 			healthPoints: 150,
 			attackPower: 15,
-			counterAttackPower: 2
+			counterAttackPower: 2,
 
 
 
@@ -38,286 +39,214 @@
 
 
 		//trying to check if a character is selected
-
-		var generic = '.generic';
-		var userCharacter = 'userCharacter';
-		var defender = 'defender';
-		var characterLevel = "#character";
-		var enemyLevel = "#enemies";
-
-
-
-		$(".generic").click(function() {
-
-
-			$(this).addClass("userCharacter");
-			$(this).removeClass("generic");
-			$(this).appendTo("#character");
-
-
-			
-		/*	$(".generic").addClass(".defender");
-			$(".defender").removeClass(".generic");
-			$(".defender").appendTo("#enemies"); */
-			
-
-			console.log($("#enemies").hasClass(".defender"));
-			
-
-
-	});
-
-	
-
-
-
-		/*
-
-
-
-
-
-		$(".character").click(function(){
-
-			var printT = $(this).attr("id");
-
-			if(printT === "luke") {
-				lukePlay();
-			}
-
-		});
-
-
-
-
+		var isCharacterChosen = false;
+		var isEnemyChosen = false;
+		var defenderPicked = false;
 		
 
 
-		// each individual game will function
 
-	
+			$(".generic").click(function() {
 
-		function lukePlay() {
-
-			
-				$("#luke").appendTo("#character");
-
-				$("#maul").appendTo("#enemies");
-				$("#obi").appendTo("#enemies");
-				$("#darth").appendTo("#enemies");
-
-				$("#darth").css("background-color", "red");
-				$("#obi").css("background-color", "red");
-				$("#maul").css("background-color", "red");
-
-				$()
+				if(!(isCharacterChosen)) {
 
 
+				$(this).addClass("userCharacter");
+				$(this).removeClass("generic");
+				$(this).appendTo("#character");
 
 
+				
+				$(".generic").addClass("defender");
+				$(".defender").removeClass("generic");
+				$(".defender").appendTo("#enemies"); 
+				
+				isCharacterChosen = true;
 
 
+				} 
 
-
-
-
-
-
-				if(!($.contains("#defender", "div"))) {
-
-					$("#maul").click(function() {
-
-						$(this).appendTo("#defender");
-						lukeFights();
-					});
-
-				} else {
-					console.log("there is already a fighter there");
-				}
-
-
+				chooseDefender();
 
 
 				
 
-			};
-
-		function lukeFights () {
-
-			$(":button").click(function() {
 
 
-				var userAttack = luke.attackPower;
-				var opponentAttack = maul.counterAttackPower;
+				});
+
+			
 
 
-				luke.healthPoints -= opponentAttack;
-				maul.healthPoints -= userAttack;
+		
 
-				luke.attackPower += 30;
+			function chooseDefender () {
 
-				$("#lukehp").html("<p>" + luke.healthPoints + "</p>");
-				$("#maulhp").html("<p>" + maul.healthPoints + "</p>");
+				$(".defender").click(function() {
 
+					if(!(isEnemyChosen)) {
 
+						$(this).appendTo("#defender");
 
-				console.log("Lukes health is: " + luke.healthPoints);
-				console.log("Maul's health is: " + maul.healthPoints);
+						isEnemyChosen = true;
+						whoIs();
+					}
 
-
-				if(luke.healthPoints <= 0) {
-
-					//this is were a will run the loser
-					console.log("Luke is defeated! You suck!");
-					return;
-				} else if (maul.healthPoints <= 0) {
-					console.log("You have defeated your opponent!");
-					$("#maul").remove();
-					lukePlay();
-				}
 
 			});
 
-
-
-
-
-
-
-		}
-
-		
-			
-
-		
-
-	
-
-
-
-
-
-		
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		// another way of possible doing this
-
-		function game() {
-
-			if($("#enemies").has("#maul")) {
-
-
-				$("#maul").click( function() {
-					$(this).appendTo("#defender");
-
-				})
-
-
-				
-			}
 		}
 
 
 
+			function whoIs () {
+
+					var userHealthPoints;
+					var userAttackPower;
+					var userCounterAttack;
+					var userHealthDisplay;
+
+					var opponentHealthPoints;
+					var opponentAttackPower;
+					var opponentCounterAttack;
+					var opHealthDisplay;
 
 
-		$("#luke").click(function() {
+					if($("#defender").children("div").attr("id") === "luke") {
 
-			if($("#character").has("div") === false) {
-				
-				$(this).appendTo("#character");
+						console.log("YOU ARE FIGHTING LUKE!");
 
-			} else {
-				$("#maul").appendTo("#enemies");
-				$("#obi").appendTo("#enemies");
-				$("#darth").appendTo("#enemies");
+						opponentHealthPoints = luke.healthPoints;
+						opponentAttackPower = luke.attackPower;
+						opponentCounterAttack = luke.counterAttackPower;
+						opHealthDisplay = $("#lukehp");
+					}
 
-				game();
-			}
+					if($("#defender").children("div").attr("id") === "obi") {
 
-		});
+						console.log("YOU ARE FIGHTING OBI!");
 
-		$("#obi").click(function() {
+						opponentHealthPoints = obi.healthPoints;
+						opponentAttackPower = obi.attackPower;
+						opponentCounterAttack = obi.counterAttackPower;
+						opHealthDisplay = $("#obihp");
+					}
 
-			if($("#character").has("div") === false) {
-				
-				$(this).appendTo("#character");
+					if($("#defender").children("div").attr("id") === "darth") {
 
-			} else {
-				$("#maul").appendTo("#enemies");
-				$("#luke").appendTo("#enemies");
-				$("#darth").appendTo("#enemies");
-			}
+						console.log("YOU ARE FIGHTING DARTH!");
 
-		});
+						opponentHealthPoints = darth.healthPoints;
+						opponentAttackPower = darth.attackPower;
+						opponentCounterAttack = darth.counterAttackPower;
+						opHealthDisplay = $("#darthhp");
+					}
 
-		$("#darth").click(function() {
+					if($("#defender").children("div").attr("id") === "maul") {
 
-			if($("#character").has("div") === false) {
-				
-				$(this).appendTo("#character");
+						console.log("YOU ARE FIGHTING MAUL!");
 
-			} else {
-				$("#obi").appendTo("#enemies");
-				$("#luke").appendTo("#enemies");
-				$("#maul").appendTo("#enemies");
-			}
-
-		});
-
-		$("#maul").click(function() {
-
-			if($("#character").has("div") === false) {
-				
-				$(this).appendTo("#character");
-
-			} else {
-				$("#obi").appendTo("#enemies");
-				$("#luke").appendTo("#enemies");
-				$("#darth").appendTo("#enemies");
-			}
-
-		});
+						opponentHealthPoints = maul.healthPoints;
+						opponentAttackPower = maul.attackPower;
+						opponentCounterAttack = maul.counterAttackPower;
+						opHealthDisplay = $("#maulhp");
+					}
 
 
+					if($("#character").children("div").attr("id") === "luke") {
+
+						console.log("THE USER CHOSE LUKE");
+
+						userHealthPoints = luke.healthPoints;
+						userAttackPower = luke.attackPower;
+						userCounterAttack = luke.counterAttackPower;
+						userHealthDisplay = $("#lukehp");
+						
+						
+					}
+
+					if($("#character").children("div").attr("id") === "obi") {
+
+						console.log("THE USER CHOSE OBI");
+
+						userHealthPoints = obi.healthPoints;
+						userAttackPower = obi.attackPower;
+						userCounterAttack = obi.counterAttackPower;
+						userHealthDisplay = $("#obihp");
+						
+						
+
+					}
+
+					if($("#character").children("div").attr("id") === "darth") {
+
+						console.log("THE USER CHOSE DARTH");
+
+						userHealthPoints = darth.healthPoints;
+						userAttackPower = darth.attackPower;
+						userCounterAttack = darth.counterAttackPower;
+						userHealthDisplay = $("#darthhp");
+						
+
+					}
+
+					if($("#character").children("div").attr("id") === "maul") {
+
+						console.log("THE USER CHOSE MAUL");
+
+						userHealthPoints = maul.healthPoints;
+						userAttackPower = maul.attackPower;
+						userCounterAttack = maul.counterAttackPower;
+						userHealthDisplay = $("#maulhp");
 			
 
+					}
 
 
+
+					$("button").click(function() {
 		
 
-		*/
+
+						userHealthPoints -= opponentCounterAttack;
+						opponentHealthPoints -= userAttackPower;
+
+						userHealthDisplay.html(userHealthPoints);
+						opHealthDisplay.html(opponentHealthPoints);
+
+						console.log("=======================================");
+
+						console.log("the opp hp is: " + opponentHealthPoints);
+
+						if(userHealthPoints <= 0) {
+
+							$("#character").remove();
+							$("#commentary").html("YOU WERE DEFEATED!").css("color", "red");
+
+
+						} else if (opponentHealthPoints <= 0) {
+
+							$("#defender").children().remove();
+							isEnemyChosen = false;
+							return;
+
+						
+
+							chooseDefender();
+
+
+							
 
 
 
+						}
 
 
+		});
 
+	}
 
+		});
 
-
- 
-
-
-
-
-});
 
 
